0c5f118991e46974d1bd58b0f64c0f267823a861
oOBobbyOo <944831575@qq.com>
2023-08-24 19:07:51 +0800

    feat: add app.config.ts and change footer

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  author: {
+    name: 'Henry Bobby',
+    links: {
+      github: 'https://github.com/oOBobbyOo/nuxtjs-template',
+    },
+    year: new Date().getFullYear(),
+  },
+})
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+const { author } = useAppConfig()
+</script>
+
 <template>
-  <footer class="w-full flex-center border-t border-gray-950/10 py-4 dark:border-gray-50/[0.2]">
-    @copyright by Henry Bobby
+  <footer
+    class="w-full flex-center border-t border-gray-950/10 py-4 dark:border-gray-50/[0.2]"
+  >
+    Copyright © {{ author?.year || new Date().getFullYear() }} by
+    {{ author?.name || '' }}. All rights reserved.
   </footer>
 </template>
