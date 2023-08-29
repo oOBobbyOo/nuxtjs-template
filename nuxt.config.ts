69afdb7ea359792c7098b48c542ca16be0d89d40
oOBobbyOo <944831575@qq.com>
2023-08-29 17:44:09 +0800

    chore: nuxt.config.ts meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const siteTitle = 'Nuxt3 starter template'
+const siteDescription = 'a starter template for Nuxt 3'
+const siteUrl = process.env.NUXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+const shareImg = `${siteUrl}/share.png`
+const twitterUser = '@bobby'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      title: siteTitle,
+      htmlAttrs: { lang: 'en' },
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      meta: [
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: siteUrl },
+        { hid: 'og:title', property: 'og:title', content: siteTitle },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: siteDescription,
+        },
+        { hid: 'og:image', property: 'og:image', content: shareImg },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        // twitter
+        { name: 'twitter:site', content: twitterUser },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'twitter:url', name: 'twitter:url', content: siteUrl },
+        { hid: 'twitter:title', name: 'twitter:title', content: siteTitle },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: siteDescription,
+        },
+        { hid: 'twitter:image', name: 'twitter:image', content: shareImg },
+      ],
     },
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'layout', mode: 'out-in' },
