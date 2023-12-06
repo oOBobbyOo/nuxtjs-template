fc18b3208c3abd19cd34ef13d9cbcf31fb9d60d9
oOBobbyOo <944831575@qq.com>
2023-12-06 12:03:18 +0800

    feat: basic bar

---
+++ src/views/charts/echarts/bar/components/BasicBar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { EChartsOption } from '@/composables/echarts'
+
+// 基础柱状图
+const options: EChartsOption = {
+  tooltip: {
+    trigger: 'axis',
+  },
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      type: 'bar',
+      data: [120, 200, 150, 80, 70, 110, 130],
+    },
+  ],
+}
+</script>
+
+<template>
+  <Echarts :options="options" />
+</template>
+
+<style scoped lang="less"></style>
--- src/views/charts/echarts/bar/index.vue
+++ src/views/charts/echarts/bar/index.vue
@@ -1,158 +1,22 @@
 <script setup lang="ts">
-import type { EChartsOption } from '@/composables/echarts'
+import BasicBar from './components/BasicBar.vue'
 
-const options: EChartsOption = {
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'shadow',
-    },
-  },
-  grid: {
-    borderWidth: 0,
-    top: 60,
-    bottom: 95,
-  },
-  legend: {
-    top: '6%',
-    textStyle: {
-      color: '#90979c',
-    },
-    data: ['女', '男', '总数'],
-  },
-  calculable: true,
-  xAxis: [
-    {
-      type: 'category',
-      axisLine: {
-        lineStyle: {
-          color: '#90979c',
-        },
-      },
-      splitLine: {
-        show: false,
-      },
-      axisTick: {
-        show: false,
-      },
-      splitArea: {
-        show: false,
-      },
-      axisLabel: {
-        interval: 0,
-      },
-      data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
-    },
-  ],
-  yAxis: [
-    {
-      type: 'value',
-      splitLine: {
-        show: false,
-      },
-      axisLine: {
-        lineStyle: {
-          color: '#90979c',
-        },
-      },
-      axisTick: {
-        show: false,
-      },
-      axisLabel: {},
-      splitArea: {
-        show: false,
-      },
-    },
-  ],
-  dataZoom: [
-    {
-      show: true,
-      height: 30,
-      xAxisIndex: [0],
-      bottom: 30,
-      start: 10,
-      end: 80,
-      handleIcon:
-        'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
-      handleSize: '110%',
-      handleStyle: {
-        color: '#d3dee5',
-      },
-      textStyle: {
-        color: '#fff',
-      },
-      borderColor: '#90979c',
-    },
-    {
-      type: 'inside',
-      show: true,
-      height: 15,
-      start: 1,
-      end: 35,
-    },
-  ],
-  series: [
-    {
-      name: '女',
-      type: 'bar',
-      stack: '总量',
-      barMaxWidth: 35,
-      barGap: '10%',
-      label: {
-        show: true,
-        color: '#fff',
-        position: 'inside',
-        formatter(p: any) {
-          return p.value > 0 ? p.value : ''
-        },
-      },
-      itemStyle: {
-        color: 'rgba(255,144,128,1)',
-      },
-      data: [709, 1917, 2455, 2610, 1719, 1433, 1544, 3285, 5208, 3372, 2484, 4078],
-    },
-
-    {
-      name: '男',
-      type: 'bar',
-      stack: '总量',
-      label: {
-        show: true,
-        color: '#fff',
-        position: 'inside',
-        formatter(p: any) {
-          return p.value > 0 ? p.value : ''
-        },
-      },
-      itemStyle: {
-        color: 'rgba(0,191,183,1)',
-      },
-      data: [327, 1776, 507, 1200, 800, 482, 204, 1390, 1001, 951, 381, 220],
-    },
-    {
-      name: '总数',
-      type: 'line',
-      symbolSize: 10,
-      symbol: 'circle',
-      label: {
-        show: true,
-        color: 'rgba(252,230,48,1)',
-        position: 'top',
-        formatter(p: any) {
-          return p.value > 0 ? p.value : ''
-        },
-      },
-      itemStyle: {
-        color: 'rgba(252,230,48,1)',
-      },
-      data: [1036, 3693, 2962, 3810, 2519, 1915, 1748, 4675, 6209, 4323, 2865, 4298],
-    },
-  ],
-}
+const colResponsiveProps = reactive({
+  xs: 24,
+  sm: 12,
+  md: 12,
+  lg: 12,
+  xl: 8,
+  style: { height: '400px' },
+})
 </script>
 
 <template>
-  <Echarts :options="options" />
+  <el-row :gutter="16">
+    <el-col v-bind="colResponsiveProps">
+      <BasicBar />
+    </el-col>
+  </el-row>
 </template>
 
-<style scoped></style>
+<style scoped lang="less"></style>
