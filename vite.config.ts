f454883923e7d4e2d05914856b3f7fe103d1f08d
oOBobbyOo <944831575@qq.com>
2023-12-20 12:04:11 +0800

    perf: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -78,7 +78,15 @@ export default defineConfig(({ command, mode }) => {
         }),
 
       // https://github.com/vbenjs/vite-plugin-compression
-      isGzip && viteCompression(),
+      isGzip
+        && viteCompression({
+          verbose: true, // 是否在控制台中输出压缩结果
+          disable: false,
+          threshold: 10240, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+          algorithm: 'gzip', // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
+          ext: '.gz',
+          deleteOriginFile: true, // 源文件压缩后是否删除(为了看压缩后的效果，选择了true)
+        }),
 
       // https://github.com/antfu/vite-plugin-inspect
       isServe
@@ -89,6 +97,46 @@ export default defineConfig(({ command, mode }) => {
       // https://github.com/webfansplz/vite-plugin-vue-devtools
       isServe && VueDevTools(),
     ],
+    // 默认启动的是 esbuild
+    esbuild: {
+      // pure: ['console.log'], // 删除 console.log
+      drop: ['console', 'debugger'], // 删除 所有的console 和 debugger
+    },
+    build: {
+      minify: 'terser', // 启用 terser 压缩， 默认为 Esbuild，它比 terser 快 20-40 倍，压缩率只差 1%-2%。
+      terserOptions: {
+        compress: {
+          // pure_funcs: ['console.log'], // 只删除 console.log
+          drop_console: true, // 删除所有 console
+          drop_debugger: true, // 删除所有 debugger
+        },
+        format: {
+          comments: false, // 删除所有注释
+        },
+      },
+      chunkSizeWarningLimit: 1024, // 规定触发警告的 chunk 大小, 以 kB 为单位, 默认 500
+      rollupOptions: {
+        output: {
+          // 最小化拆分包
+          // manualChunks(id) {
+          //   if (id.includes('node_modules')) {
+          //     return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          //   }
+          // },
+
+          // 单独拆分
+          manualChunks: {
+            'vue-venders': ['vue', 'vue-router'],
+            'echarts': ['echarts', 'echarts-liquidfill'],
+            'lodash': ['lodash-es'],
+          },
+          chunkFileNames: 'assets/js/[name]-[hash].js', // 引入文件名的名称
+          entryFileNames: 'assets/js/[name]-[hash].js', // 包的入口文件名称
+          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+          experimentalMinChunkSize: 10 * 1024, // 单位b 没有副作用，合并较小的模块
+        },
+      },
+    },
     resolve: {
       alias: {
         '~': resolve(process.cwd()),
