23629a29e2339bd747d322f574388e100479c986
oOBobbyOo <944831575@qq.com>
2024-01-16 18:06:59 +0800

    refactor: change useG2Chart theme

--- src/hooks/web/useG2Chart.ts
+++ src/hooks/web/useG2Chart.ts
@@ -1,9 +1,19 @@
 import { Chart } from '@antv/g2'
+import { useAppSetting } from '@/hooks/setting/useAppSetting'
 
 export { Chart }
 
 export function useG2Chart(elRef: Ref<HTMLDivElement>) {
   let chartInstance: Chart | null = null
+  const { getDarkMode } = useAppSetting()
+  const theme = computed(() => {
+    return {
+      type: unref(getDarkMode),
+      view: {
+        viewFill: 'transparent',
+      },
+    }
+  })
 
   function initCharts() {
     const el = unref(elRef)
@@ -14,6 +24,12 @@ export function useG2Chart(elRef: Ref<HTMLDivElement>) {
       container: el,
       autoFit: true,
     })
+    chartInstance.theme(theme.value)
+  }
+
+  function updateCharts() {
+    chartInstance?.theme(theme.value)
+    chartInstance?.render()
   }
 
   function getInstance(): Chart | null {
@@ -37,6 +53,13 @@ export function useG2Chart(elRef: Ref<HTMLDivElement>) {
   onMounted(() => {
     initCharts()
   })
+
+  watch(
+    () => theme.value,
+    () => {
+      updateCharts()
+    },
+  )
 
   tryOnUnmounted(() => {
     if (!chartInstance)
