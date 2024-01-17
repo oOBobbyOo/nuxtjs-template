3525f9f9c5a9686048f733a80bff5c123edb071e
oOBobbyOo <944831575@qq.com>
2024-01-17 10:35:30 +0800

    refactor: change ReloadButton

--- src/layouts/components/MultipleTabs/ReloadButton.vue
+++ src/layouts/components/MultipleTabs/ReloadButton.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { useI18n } from '@/hooks/web/useI18n'
+import { useAppStore } from '@/stores/modules/app'
 
 defineOptions({ name: 'ReloadButton' })
 
 const { t } = useI18n()
+
+const app = useAppStore()
 </script>
 
 <template>
-  <div class="h-full w-64px flex-center cursor-pointer hover:bg-#f6f6f6 dark:hover:bg-#333">
+  <div class="h-full w-64px flex-center cursor-pointer hover:bg-#f6f6f6 dark:hover:bg-#333" @click="app.reloadPage">
     <el-tooltip placement="bottom" :content="t('layout.multipleTab.reload')">
       <Icon size="22" icon="mdi:refresh" />
     </el-tooltip>
